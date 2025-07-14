0fb41c676ec73b221f469c00c49ab9367647f361
Lara Luchi <llaraluchi@gmail.com>
2025-07-14 16:51:04 -0300

    alerta para CPF já cadastrado

--- ta-gui/src/app/app.ts
+++ ta-gui/src/app/app.ts
@@ -22,6 +22,7 @@ export class App {
        this.aluno = {nome: "", cpf: "", email: ""};
       } else {
         this.aluno.cpf = "";
+        alert("Já existe um aluno com esse CPF");
       }
      }
     
